acd3fc1b337508edbebafa8b9cb4ece6e3dc0aca
kdiab <khalid@diab.cc>
2024-07-07 20:54:10 -0400

    Remove broken editor, start new one

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,9 @@
-import "./App.css";
-
-import Editor from "./components/Editor";
-import ExampleDocument from "./utils/ExampleDocument";
-import Navbar from "react-bootstrap/Navbar";
-import { useState } from "react";
+import Editor from './components/Editor.jsx';
 
 function App() {
-  const [document, updateDocument] = useState(ExampleDocument);
 
   return (
-    <>
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#">
-          Editor
-        </Navbar.Brand>
-      </Navbar>
-      <div className="App">
-        <Editor document={document} onChange={updateDocument} />
-      </div>
-    </>
+    <Editor />
   );
 }
 
