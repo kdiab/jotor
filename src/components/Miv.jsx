b2f9618159f18fe8a21f8dced827bd4c0dcbfbba
kdiab <khalid@diab.cc>
2024-07-11 21:38:23 -0400

    add more headings

--- src/components/Miv.jsx
+++ src/components/Miv.jsx
@@ -28,18 +28,36 @@ const Blocks = ({ attributes, children, element }) => {
           {children}
         </ul>
       );
-    case 'heading-one':
+    case 'h1':
       return (
         <h1 style={style} {...attributes}>
           {children}
         </h1>
       );
-    case 'heading-two':
+    case 'h2':
       return (
         <h2 style={style} {...attributes}>
           {children}
         </h2>
       );
+    case 'h3':
+          return (
+            <h3 style={style} {...attributes}>
+              {children}
+            </h3>
+          );
+    case 'h4':
+          return (
+            <h4 style={style} {...attributes}>
+              {children}
+            </h4>
+          );
+    case 'h5':
+          return (
+            <h5 style={style} {...attributes}>
+              {children}
+            </h5>
+          );
     case 'list-item':
       return (
         <li style={style} {...attributes}>
@@ -188,8 +206,11 @@ const Miv = () => {
         }
       }}
     >
-      <BlockButton format="heading-one" />
-      <BlockButton format="heading-two" />
+      <BlockButton format="h1" />
+      <BlockButton format="h2" />
+      <BlockButton format="h3" />
+      <BlockButton format="h4" />
+      <BlockButton format="h5" />
       <BlockButton format="block-quote" />
       <BlockButton format="numbered-list" />
       <BlockButton format="bulleted-list" />
